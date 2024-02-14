68f045d379de0e811b04214532c685de375a2d74
chc <311mikecho@gmail.com>
2024-02-14 17:29:19 +0900

    class12

---
+++ src/classTwelve/TwoContainer.java
@@ -0,0 +1,16 @@
+package classTwelve;
+
+public class TwoContainer <K, V>{
+    private K key;
+    private V value;
+    public K getKey() {
+        return key;
+    }
+    public V getValue(){
+        return value;
+    }
+    public void set(K key, V value){
+        this.key = key;
+        this.value = value;
+    }
+}
