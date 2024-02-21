c5993ba1c2fd2389df0bff98705ded3be59bdc0c
chc <311mikecho@gmail.com>
2024-02-21 10:41:14 +0900

    class16

---
+++ src/classSixteen/mainSixteen.java
@@ -0,0 +1,11 @@
+package classSixteen;
+
+import java.util.*;
+
+public class mainSixteen {
+    List<String> list = Arrays.asList("e", "a", "c", "z", "d");
+
+//        list.stream()
+//                .sorted()
+//                .forEach(System.out::println);//오름차순
+}
